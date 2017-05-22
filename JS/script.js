ecbf441b96a222eafca8b35875e6440bad19e218
RenatMG <kaverina2@yandex.ru>
2017-05-22 16:53:40 +0400

    3

--- JS/script.js
+++ JS/script.js
@@ -2,5 +2,5 @@ var color = '#F0F8FF #979CA0 sdsd: 32dcd2 sddsdss  ,#97A4A0';
 color = color.match(/#[A-F0-9]{6}/g);
 console.log(color);
 var number = '1.5 0 12. 123.4.';
-number = number.match(/[0-9]+(\.([0-9])+)?/);
+number = number.match(/[0-9]+(\.([0-9])+)?/g);
 console.log(number);
