1fbcdb6dbe53ec1f59dd247c6c734dac9188cd3a
RenatMG <kaverina2@yandex.ru>
2017-05-22 18:06:07 +0400

    5

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,10 @@
+'use strict';
 var color = '#F0F8FF #979CA0 sdsd: 32dcd2 sddsdss  ,#97A4A0';
 color = color.match(/#[A-F0-9]{6}/g);
 console.log(color);
 var number = '1.5 0 12. h  g 445  g 0.05 123.4fg.';
-number = number.match(/[0-9]+(\.([0-9])+)?/g);
+number = number.match(/\d+(\.\d+)?/g); //([0-9]+(\.([0-9])+)?)
+number = number.filter(function (number) {
+    return number > 0;
+});
 console.log(number);
