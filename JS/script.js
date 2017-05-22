b165d61b474b1e63ab574440dc4eab807c630190
RenatMG <kaverina2@yandex.ru>
2017-05-22 16:55:44 +0400

    4

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 var color = '#F0F8FF #979CA0 sdsd: 32dcd2 sddsdss  ,#97A4A0';
 color = color.match(/#[A-F0-9]{6}/g);
 console.log(color);
-var number = '1.5 0 12. 123.4.';
+var number = '1.5 0 12. h  g 445  g 0.05 123.4fg.';
 number = number.match(/[0-9]+(\.([0-9])+)?/g);
 console.log(number);
