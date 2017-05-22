e2da83c600dd2cf5bca219992a33dd681a9fcc12
RenatMG <kaverina2@yandex.ru>
2017-05-22 16:51:04 +0400

    1

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
 var color = '#F0F8FF #979CA0 sdsd: 32dcd2 sddsdss  ,#97A4A0';
-color = color.match(/#[A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9]/g);
+color = color.match(/#[A-F0-9]{6}/g);
 console.log(color);
+var number = '1.5 0 12. 123.4.';
+number = number.match([0 - 9] + (\.([0 - 9]) + ) ? );
+console.log(number);
